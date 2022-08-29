23c36a164fb3734c967ab55e01e7481c14b8b24e
Elisabeth V <76956254+chocolateofpain@users.noreply.github.com>
2022-08-29 20:04:37 +0200

    add dependencies

--- __checks__/myFirstTest.js
+++ __checks__/myFirstTest.js
@@ -8,3 +8,10 @@ console.log('Killer Bunny is doing syncing in a folder')
 killerBunnyHelper()
 nestedHelper1()
 nestedHelper2()
+
+const playwright = require('playwright')
+const assert = require('assert')
+
+const browser = await playwright.chromium.launch()
+const page = await browser.newPage()
+await page.goto('https://www.google.com/')
